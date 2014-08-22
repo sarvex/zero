abe3061e76320460900d27f2a716b1a10b9222be
sunil.jakhar@fareportal.com <sunil@sunil-jakhar-iMac.local>
2014-08-22 20:04:39 +0530

    Updated Code

--- Zero/Zero/ViewController.m
+++ Zero/Zero/ViewController.m
@@ -10,10 +10,9 @@
 #import "SecondViewController.h"
 #import "SelectorViewController.h"
 
-@interface ViewController ()<UIPickerViewDataSource,UIPickerViewDelegate, SelectorViewControllerDelegate>
+@interface ViewController ()<SelectorViewControllerDelegate>
 {
-    NSArray *_pickerData;
-    NSString *airlineName;
+
 }
 @property(nonatomic,strong) SelectorViewController *selectorVC;
 @end
@@ -30,34 +29,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-
-// The number of columns of data
-- (long)numberOfComponentsInPickerView:(UIPickerView *)pickerView
-{
-    return 1;
-}
-
-// The number of rows of data
-- (long)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
-{
-    return _pickerData.count;
-}
-
-// The data to return for the row and component (column) that's being passed in
-- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-{
-    return _pickerData[row];
-}
-
-//- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
-//{
-//    airlineName=[_pickerData objectAtIndex:row];
-//    _pickerView.hidden=YES;
-//    _button.hidden=NO;
-//    [_button setTitle:airlineName forState:UIControlStateNormal];
-//    // This method is triggered whenever the user makes a change to the picker selection.
-//    // The parameter named row and component represents what was selected.
-//}
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([segue.identifier isEqualToString:@"selector"]) {
@@ -89,13 +60,4 @@
     return mystoryboard;
 }
 
-- (IBAction)didBtnClicked:(id)sender
-{
-//    self.pickerView.hidden=NO;
-//    _button.hidden=YES;
-}
-- (IBAction)didFlightBtnClicked:(id)sender
-{
-    
-}
 @end
